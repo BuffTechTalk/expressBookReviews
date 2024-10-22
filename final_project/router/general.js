acf0fb513c95211bd5358500f29581ff7b16108d
Carl <czhang@wtamu.edu>
2024-10-22 01:54:54 -0400

    complete task1-6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,54 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const author = req.params.author;
+    let result = {};
+    
+    // Loop through each book in the books object
+    for (let id in books) {
+        // Check if the author matches
+        if (books[id].author === author) {
+            result[id] = books[id];
+        }
+    }
+    
+    res.send(JSON.stringify(result, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let result = {};
+  
+  // Loop through each book in the books object
+  for (let id in books) {
+      // Check if the title matches
+      if (books[id].title === title) {
+          result[id] = books[id];
+      }
+  }
+  
+  res.send(JSON.stringify(result, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
